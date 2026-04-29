6a7956d645dcac00496662e409a4849a08ce9885
IgnacioTejeraPicossi <ignacio.tejera@outlook.com>
2026-04-29 07:40:54 +0200

    Translate 'Run Test' to spanish

--- frontend/src/i18n/index.js
+++ frontend/src/i18n/index.js
@@ -96,6 +96,7 @@ import esFutureAppModule from './locales/es/futureAppModule.json';
 import esIdeaLogModule from './locales/es/ideaLogModule.json';
 import esFeatureRoadmapModule from './locales/es/featureRoadmapModule.json';
 import esApiConfigModule from './locales/es/apiConfigModule.json';
+import esRunTestModule from './locales/es/runTestModule.json';
 
 i18n
   .use(LanguageDetector)
@@ -120,7 +121,7 @@ i18n
         common: { ...noCommon, ...noRunTest, ...noAppChrome, ...noMicroLesson, ...noScenarioSimulator, ...noSimulationResults, ...noTeamDynamics, ...noCertificationsModule, ...noCareerCoachModule, ...noSkillsForecastModule, ...noBabelLibraryModule, ...noKnowledgeMapModule, ...noRepoAnalyzerModule, ...noAgentCursorModule, ...noAgenticRagModule, ...noLearningRepoModule, ...noDocumentAnalyzerModule, ...noLearningDocumentModule, ...noAgenticRagDocumentModule, ...noEaSecondBrainModule, ...noSalesAssistantModule, ...noPersonalAttentionAgentModule, ...noTelcoOpsAgentModule, ...noGrcAgentModule, ...noCouncilAgentModule, ...noOpsEfficiencyAgentModule, ...noAtmCopilotModule, ...noApiConfigModule, ...noFutureAppModule, ...noIdeaLogModule, ...noFeatureRoadmapModule, ...noCloudInstallModule, ...noAgiHubModule, ...noWebSearchModule }
       },
       es: {
-        common: { ...esCommon, ...esAppChrome, ...esMicroLesson, ...esScenarioSimulator, ...esSimulationResults, ...esTeamDynamics, ...esCertificationsModule, ...esCareerCoachModule, ...esSkillsForecastModule, ...esKnowledgeMapModule, ...esBabelLibraryModule, ...esWebSearchModule, ...esRepoAnalyzerModule, ...esAgentCursorModule, ...esLearningRepoModule, ...esDocumentAnalyzerModule, ...esLearningDocumentModule, ...esAgenticRagDocumentModule, ...esEaSecondBrainModule, ...esAgiHubModule, ...esCloudInstallModule, ...esFutureAppModule, ...esIdeaLogModule, ...esFeatureRoadmapModule, ...esApiConfigModule }
+        common: { ...esCommon, ...esAppChrome, ...esMicroLesson, ...esScenarioSimulator, ...esSimulationResults, ...esTeamDynamics, ...esCertificationsModule, ...esCareerCoachModule, ...esSkillsForecastModule, ...esKnowledgeMapModule, ...esBabelLibraryModule, ...esWebSearchModule, ...esRepoAnalyzerModule, ...esAgentCursorModule, ...esLearningRepoModule, ...esDocumentAnalyzerModule, ...esLearningDocumentModule, ...esAgenticRagDocumentModule, ...esEaSecondBrainModule, ...esAgiHubModule, ...esCloudInstallModule, ...esFutureAppModule, ...esIdeaLogModule, ...esFeatureRoadmapModule, ...esApiConfigModule, ...esRunTestModule }
       }
     }
   });
