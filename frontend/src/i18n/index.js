07bdc94f070d4d710642214e7d34375e037a309b
IgnacioTejeraPicossi <ignacio.tejera@outlook.com>
2026-05-11 11:41:37 +0200

    Translate to spanish agent 'Sales Assistant Agent'

--- frontend/src/i18n/index.js
+++ frontend/src/i18n/index.js
@@ -93,6 +93,7 @@ import esDocumentAnalyzerModule from './locales/es/documentAnalyzerModule.json';
 import esLearningDocumentModule from './locales/es/learningDocumentModule.json';
 import esAgenticRagDocumentModule from './locales/es/agenticRagDocumentModule.json';
 import esEaSecondBrainModule from './locales/es/eaSecondBrainModule.json';
+import esSalesAssistantModule from './locales/es/salesAssistantModule.json';
 import esAgiHubModule from './locales/es/agiHubModule.json';
 import esCloudInstallModule from './locales/es/cloudInstallModule.json';
 import esFutureAppModule from './locales/es/futureAppModule.json';
@@ -126,7 +127,7 @@ i18n
         common: { ...noCommon, ...noRunTest, ...noAppChrome, ...noMicroLesson, ...noScenarioSimulator, ...noSimulationResults, ...noTeamDynamics, ...noCertificationsModule, ...noCareerCoachModule, ...noSkillsForecastModule, ...noBabelLibraryModule, ...noKnowledgeMapModule, ...noRepoAnalyzerModule, ...noAgentCursorModule, ...noAgenticRagModule, ...noLearningRepoModule, ...noDocumentAnalyzerModule, ...noLearningDocumentModule, ...noAgenticRagDocumentModule, ...noEaSecondBrainModule, ...noSalesAssistantModule, ...noPersonalAttentionAgentModule, ...noTelcoOpsAgentModule, ...noGrcAgentModule, ...noCouncilAgentModule, ...noOpsEfficiencyAgentModule, ...noAtmCopilotModule, ...noRedCrossWebQaModule, ...noApiConfigModule, ...noFutureAppModule, ...noIdeaLogModule, ...noFeatureRoadmapModule, ...noCloudInstallModule, ...noAgiHubModule, ...noWebSearchModule }
       },
       es: {
-        common: { ...esCommon, ...esAppChrome, ...esMicroLesson, ...esScenarioSimulator, ...esSimulationResults, ...esTeamDynamics, ...esCertificationsModule, ...esCareerCoachModule, ...esSkillsForecastModule, ...esKnowledgeMapModule, ...esBabelLibraryModule, ...esWebSearchModule, ...esRepoAnalyzerModule, ...esAgentCursorModule, ...esLearningRepoModule, ...esDocumentAnalyzerModule, ...esLearningDocumentModule, ...esAgenticRagDocumentModule, ...esEaSecondBrainModule, ...esAgiHubModule, ...esCloudInstallModule, ...esFutureAppModule, ...esIdeaLogModule, ...esFeatureRoadmapModule, ...esApiConfigModule, ...esRunTestModule, ...esRobomindClinicModule, ...esAtmCopilotModule, ...esRedCrossWebQaModule }
+        common: { ...esCommon, ...esAppChrome, ...esMicroLesson, ...esScenarioSimulator, ...esSimulationResults, ...esTeamDynamics, ...esCertificationsModule, ...esCareerCoachModule, ...esSkillsForecastModule, ...esKnowledgeMapModule, ...esBabelLibraryModule, ...esWebSearchModule, ...esRepoAnalyzerModule, ...esAgentCursorModule, ...esLearningRepoModule, ...esDocumentAnalyzerModule, ...esLearningDocumentModule, ...esAgenticRagDocumentModule, ...esEaSecondBrainModule, ...esSalesAssistantModule, ...esAgiHubModule, ...esCloudInstallModule, ...esFutureAppModule, ...esIdeaLogModule, ...esFeatureRoadmapModule, ...esApiConfigModule, ...esRunTestModule, ...esRobomindClinicModule, ...esAtmCopilotModule, ...esRedCrossWebQaModule }
       }
     }
   });
